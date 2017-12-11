de9104ff2224cfc29933ebcef5476d9977d6903d
Austin Tsiaperas <liketexas@gmail.com>
2017-12-10 18:37:37 -0600

    revised script to account for incorrect keystrokes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,52 @@
 var dictionary = ["decker","cyberdyne","replicant"];
-var guessed;
+var guesses;
 var word;
 var solution;
 var inWord;
+var alreadyGuessed;
 var chances;
+var isLetter;
+var correctGuesses;
 
 function newGame(){
 	word = dictionary[Math.floor(Math.random()*dictionary.length)];
-	guessed = [];
+	guesses = [];
 	solution = [];
 	for (i = 0; i < word.length; i++)
 	{
 		solution.push("_");
 	}
 	chances = 6;
+	correctGuesses = 0;
 	drawScreen();
 };
 
 function checkLetter(letter){
     inWord = false;
-    for (i=0; i<word.length; i++){
-    	if (letter == word[i]){
-    		inWord = true;
-    		solution[i]=letter.toUpperCase();
-    	}
+    alreadyGuessed = false;
+
+    for (i=0; i<guesses.length; i++)
+    {
+         if (letter == guesses[i]){
+         	alreadyGuessed = true;
+         }
+    }
+    
+    if (!alreadyGuessed){
+        
+        guesses.push(letter.toUpperCase());
+        for (i=0; i<word.length; i++){
+    	    if (letter == word[i]){
+    		    inWord = true;
+    		    solution[i]=letter.toUpperCase();
+    		    correctGuesses += 1;
+    	        }
+        }
     }
 };
 
 function drawScreen(){
-	$("#guessed").text(guessed);
+	$("#guessed").text(guesses);
 	$("#word").text(solution);
 	$("footer").text(chances + " guesses remaining!");
 }
@@ -36,15 +54,18 @@ function drawScreen(){
 $(document).ready(function(){
  newGame();
  document.onkeyup = function(event) {
-  	guessed.push(event.key.toUpperCase());
-    checkLetter(event.key);
-    if (!inWord){
+  	console.log(event.keyCode);
+  	if(64<event.keyCode && event.keyCode<91){
+        checkLetter(event.key);
+        if (!inWord){
         chances -=1;
-    }
-    if (chances <= 0){
-    	newGame();
+        }
     }
     drawScreen();
+    if (chances <= 0 || correctGuesses == word.length){
+    	newGame();
+    }
+   
  };
 
 
